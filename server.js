1c5e2abc4665367287137fea03125a629a117f97
reuvencf <rcerfabian@gmail.com>
2023-11-15 11:56:53 +0200

    update git ignore + locall host correct oath

--- server.js
+++ server.js
@@ -10,24 +10,26 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5174",
+        origin: "http://localhost:5173",
         methods: ["GET", "POST"],
     },
 });
-
+let userList = []
 io.on("connection", (socket) => {
     console.log(`User Connected: ${socket.id}`);
-
+    userList.push(socket.id)
+    console.log(userList)
     socket.on("join_room", (data) => {
         socket.join(data);
     });
 
     socket.on("send_message", (data) => {
-        socket.to(data.room).emit("receive_message", data);
+        console.log("room:",data.room,"message:",data.message)
+        socket.to(data.room).emit("receive_message", data.message);
     });
 });
 
 
 server.listen(3001, () => {
-    console.log("server is runing");
+    console.log("server is running");
 })
