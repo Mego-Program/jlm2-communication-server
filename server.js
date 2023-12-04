a617a57e4c35c1a42fcdf71ea09e84eb2ba75ee8
reuvencf <rcerfabian@gmail.com>
2023-12-04 11:44:32 +0200

    changing from general to specipic adresses in the origin

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const http = require("http");
 const { Server } = require('socket.io');
 const cors = require("cors");
+const { types } = require('util');
+const { type } = require('os');
 
 app.use(cors());
 
@@ -11,19 +13,27 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: "*",
-        //origin: "http://localhost:5173",
+        origin: ["http://localhost:5174","http://localhost:5173","http://localhost:4173","https://communication-jlm-2-9b72.vercel.app/"],
         methods: ["GET", "POST"],
     }
 });
 let userList = []
+let rooms = ['100','200','300']
+
+// rooms.map((object) =>io.join(object))
+
+
 io.on("connection", (socket) => {
     console.log(`User Connected: ${socket.id}`);
     userList.push(socket.id)
-    console.log(userList)
+    console.log(userList, userList.length)
+
     socket.on("join_room", (data) => {
         socket.join(data);
+        console.log(data)
     });
+
+
 
     socket.on("send_message", (data) => {
         console.log(data)
@@ -34,8 +44,11 @@ io.on("connection", (socket) => {
             socket.to(data.room).emit("receive_message", data)
         }  
     });
-
-
+    socket.on('disconnect', ()=>{
+        console.log(`   User Disonnected: ${socket.id}`);
+        userList = userList.filter((socketID) => socketID != socket.id)
+        console.log(userList,userList.length)
+    })
 });
 
 
